023278d5d7e7a878410cd90028b75d7daa365009
warriorbob <mailinglists@thefighting.org>
2013-01-09 12:41:20 -0800

    Added some early objects and such, just playing around for now.

---
+++ dmg/Domain/ScreenGrid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmg.Domain
+{
+    /// <summary>
+    /// Represents a grid of tiles
+    /// </summary>
+    class ScreenGrid
+    {
+        public List<Tile>[,] Grid { get; set; }
+
+        public ScreenGrid(int width, int height)
+        {
+            Grid = new List<Tile>[width, height];
+            foreach (List<Tile> tileList in Grid)
+            {
+                tileList.Add(new Tile { Char = '.' });
+            }
+        }
+    }
+}
--- dmg/Program.cs
+++ dmg/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dmg.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,20 @@ namespace dmg
 {
     class Program
     {
+        private const int CONSOLE_WIDTH = 80;
+        private const int CONSOLE_HEIGHT = 25;
+
         static void Main(string[] args)
         {
+            ScreenGrid screenGrid = new ScreenGrid(CONSOLE_WIDTH, CONSOLE_HEIGHT);
+
+            for(int i = 0; i < CONSOLE_WIDTH; i++)
+            {
+                for (int j = 0; j < CONSOLE_HEIGHT; j++)
+                {
+
+                }
+            }
         }
     }
 }

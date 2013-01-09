96cc19346469c5d9f45a2734c97ff5e49cf81e79
warriorbob <mailinglists@thefighting.org>
2013-01-09 15:28:56 -0800

    Added Dude.

---
+++ dmg/Domain/Dude.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmg.Domain
+{
+    public class Dude
+    {
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Dude(int x, int y)
+        {
+            XPos = x;
+            YPos = y;
+            Color = ConsoleColor.Red;
+        }
+    }
+}

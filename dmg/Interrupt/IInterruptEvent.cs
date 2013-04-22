808ad423f4e2b8c03622bc4356b28d5800c5d615
warriorbob <mailinglists@thefighting.org>
2013-04-22 12:28:03 -0700

    Changed IInterruptEvent.cs filename to match the interface name.

---
+++ dmg/Interrupt/IInterruptEvent.cs
@@ -0,0 +1,14 @@
+﻿using dmg.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmg.Interrupt
+{
+    public interface IInterruptEvent
+    {
+        void DoStuff(Queue<IInterruptEvent> queue, StateManager state, ref Map map);
+    }
+}

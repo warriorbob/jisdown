6ade4867dd6cf06daae322381fe8ee4d1355b163
warriorbob <mailinglists@thefighting.org>
2013-08-01 14:40:56 -0700

    Put some randomness into deathspray

--- dmg/Domain/Baddie.cs
+++ dmg/Domain/Baddie.cs
@@ -83,13 +83,33 @@ namespace dmg.Domain
             int xDirectionSign = Math.Sign(XPos - fromX);
             int yDirectionSign = Math.Sign(YPos - fromY);
 
-            map.StainTile(XPos, YPos, new RedStain(3)); // Baddie location
-            map.StainTile(XPos + xDirectionSign, YPos + yDirectionSign, new RedStain(2)); //Right next
-            map.StainTile(XPos + 2 * xDirectionSign, YPos + 2 * yDirectionSign, new RedStain(0)); //two out
-            map.StainTile(XPos + xDirectionSign + yDirectionSign, YPos + yDirectionSign + xDirectionSign,new RedStain(1)); //one side
-            map.StainTile(XPos + xDirectionSign - yDirectionSign, YPos + yDirectionSign - xDirectionSign, new RedStain(1)); //other side
-            map.StainTile(XPos + 2 * xDirectionSign + yDirectionSign, YPos + 2 * yDirectionSign + xDirectionSign, new RedStain(0)); //2 out, one side
-            map.StainTile(XPos + 2 * xDirectionSign - yDirectionSign, YPos + 2 * yDirectionSign - xDirectionSign,new RedStain(0)); //2 out, other side 
+            Random rand = new Random();
+            int randInt = rand.Next(100);
+
+            if (randInt <= 60)
+            {
+                map.StainTile(XPos, YPos, new RedStain(2)); // Baddie location
+                map.StainTile(XPos + xDirectionSign, YPos + yDirectionSign, new RedStain(0)); //Right next
+            }
+
+            else if (randInt <= 90)
+            {
+                map.StainTile(XPos, YPos, new RedStain(2)); // Baddie location
+                map.StainTile(XPos + xDirectionSign, YPos + yDirectionSign, new RedStain(1)); //Right next
+                map.StainTile(XPos + 2 * xDirectionSign, YPos + 2 * yDirectionSign, new RedStain(0)); //two out
+                map.StainTile(XPos + xDirectionSign + yDirectionSign, YPos + yDirectionSign + xDirectionSign, new RedStain(0)); //one side
+                map.StainTile(XPos + xDirectionSign - yDirectionSign, YPos + yDirectionSign - xDirectionSign, new RedStain(0)); //other side
+            }
+            else if (randInt <= 98)
+            {
+                map.StainTile(XPos, YPos, new RedStain(3)); // Baddie location
+                map.StainTile(XPos + xDirectionSign, YPos + yDirectionSign, new RedStain(2)); //Right next
+                map.StainTile(XPos + 2 * xDirectionSign, YPos + 2 * yDirectionSign, new RedStain(0)); //two out
+                map.StainTile(XPos + xDirectionSign + yDirectionSign, YPos + yDirectionSign + xDirectionSign, new RedStain(1)); //one side
+                map.StainTile(XPos + xDirectionSign - yDirectionSign, YPos + yDirectionSign - xDirectionSign, new RedStain(1)); //other side
+                map.StainTile(XPos + 2 * xDirectionSign + yDirectionSign, YPos + 2 * yDirectionSign + xDirectionSign, new RedStain(0)); //2 out, one side
+                map.StainTile(XPos + 2 * xDirectionSign - yDirectionSign, YPos + 2 * yDirectionSign - xDirectionSign, new RedStain(0)); //2 out, other side 
+            }
 
             Alive = false;
         }
